dfeaa25280af5480b20acf5e891631725b97360f
Tu nombre <jessica.berrio2104@gmail.com>
2023-03-29 08:02:37 -0500

    condig: API KEY, add: view modules, view only one module and generate text

---
+++ index.js
@@ -0,0 +1,78 @@
+const { Configuration, OpenAIApi } = require("openai");
+//require('dotenv').config();
+
+/**se crea una instancia de la clase Configuration con la información necesaria para
+ *  realizar la autenticación con la API de OpenAI. En este caso, se está utilizando
+ *  la variable de entorno OPENAI_API_KEY, que se supone que debe contener
+ *  la clave de la API de OpenAI.
+ */
+const configuration = new Configuration({
+    /**La variable de entorno process.env es un objeto global que proporciona
+     *  acceso a todas las variables de entorno definidas en el sistema operativo.
+     *  En este caso, se busca la variable OPENAI_API_KEY y se utiliza como valor
+     *  para la propiedad apiKey del objeto de configuración. */
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+    /**se crea una instancia de la clase OpenAIApi, pasándole la instancia de
+     *  Configuration creada anteriormente como argumento. Esto configura la 
+     * conexión con los servicios de OpenAI, permitiendo que la aplicación
+     *  pueda hacer solicitudes a la API de OpenAI. */
+    const openai = new OpenAIApi(configuration);
+
+/**MODELO:Un modelo en OpenAI es un programa de inteligencia artificial que ha
+ *  sido entrenado con grandes cantidades de datos para aprender a realizar 
+ * tareas específicas */
+
+/** LISTAR MODELOS:
+ * Enumera los modelos disponibles actualmente y proporciona información básica
+ * sobre cada uno, como el propietario y la disponibilidad. */
+async function listarModelos() {
+    const response = await openai.listModels();
+    console.log(response);
+}
+//listarModelos();
+
+/**INFORMACION DE UN MODELO ESPECIFICO
+ * Recupera una instancia modelo, proporcionando información básica sobre el modelo, 
+ * como el propietario y los permisos. */
+async function recuperarModelos() {
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.retrieveModel("text-davinci-003");
+    console.log(response);
+}
+
+//recuperarModelos();
+
+/**CREAR TEXTO:
+ * Genera un texto dependiendo del contenido del cuerpo de la solicitud
+ */
+async function generarTexto() {
+    try{
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            /**es el texto inicial que se proporciona al modelo de lenguaje
+             *  de OpenAI para generar el texto de salida. */
+            prompt: "My name is Sofia, i'm 34 years old, i'm developer with 5 experience years please create a decription for a cv",
+            /**limitar el número máximo de tokens (o palabras) que se 
+             * generarán como respuesta. */
+            max_tokens: 50, 
+            /**Valores más altos como 0.8 harán que la salida sea más aleatoria, mientras 
+             * que valores más bajos como 0.2 la harán más enfocada */
+            temperature: 0,
+            /**Controla la creatividad y diversidad del texto generado */
+            top_p: 1,
+            /**es un número entero que indica la cantidad de respuestas 
+             * únicas que se generarán. devuelve una lista con n elementos, cada uno de los
+             *  cuales representa una respuesta generada por el modelo de lenguaje. */
+            n: 1,
+            stream: false,
+            logprobs: null,
+            stop: "\n"
+          });
+          console.log(response);
+    }catch(err){
+    console.log(err);
+    }
+}
+generarTexto();
+      
